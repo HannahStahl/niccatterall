7ebb109ed89ebe92933df80ebcc0a2bb2c494b9b
Hannah Stahl <hls62@cornell.edu>
2018-12-31 10:35:12 -0600

    Darkened Instagram photo backgrounds on hover

---
+++ src/components/Photo.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import '../styles/Photo.css';
+
+class Photo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false
+    };
+    this.toggleHover = this.toggleHover.bind(this);
+  }
+
+  toggleHover() {
+    this.setState({ hover: !this.state.hover });
+  }
+
+  render() {
+    return (
+      <a
+        href={this.props.photo.link}
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          "backgroundImage": this.state.hover ? `linear-gradient(
+            rgba(0, 0, 0, 0.5), 
+            rgba(0, 0, 0, 0.5)
+          ), url(${this.props.photo.images.standard_resolution.url})` : `linear-gradient(
+            rgba(0, 0, 0, 0.1), 
+            rgba(0, 0, 0, 0.1)
+          ), url(${this.props.photo.images.standard_resolution.url})`
+        }}
+        className="instagram-photo"
+        alt="Instagram"
+        onMouseEnter={this.toggleHover}
+        onMouseLeave={this.toggleHover}
+      >
+        <div className="likes">
+          <img src="heart.svg" width="28px" alt="Likes" />
+          <p>{this.props.photo.likes.count}</p>
+        </div>
+        <div className="comments">
+          <p>{this.props.photo.comments.count}</p>
+          <img src="speech-bubble.svg" width="28px" alt="Comments" />
+        </div>
+      </a>
+    );
+  }
+}
+
+export default Photo;
--- src/components/Social.js
+++ src/components/Social.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import config from '../config.js';
+import Photo from './Photo';
 import '../styles/Social.css';
 
 class Social extends Component {
@@ -34,34 +35,7 @@ class Social extends Component {
           this.state.photos.length > 0 ?
           <div className="instagram-photos">
             { this.state.photos.map(photo =>
-              <a
-                href={photo.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                key={photo.id}
-                style={{
-                  "backgroundImage": `linear-gradient(
-                    rgba(49, 49, 49, 0.1), 
-                    rgba(49, 49, 49, 0.1)
-                  ), url(${photo.images.standard_resolution.url})`
-                }}
-                className="instagram-photo"
-                alt="Instagram"
-              >
-                <div className="likes">
-                  <img src="heart.svg" width="28px" alt="Likes" />
-                  <p>{photo.likes.count}</p>
-                </div>
-                <div className="comments">
-                  <p>{photo.comments.count}</p>
-                  <img src="speech-bubble.svg" width="28px" alt="Comments" />
-                </div>
-              </a>
-              // TODO white should stay white on hover
-              // could do this by extracting each photo into separate component
-              // (which is probably worth doing anyway at this point for code cleanliness)
-              // each of these components will have its own state
-              // so then you could use onmouseenter hover state solution
+              <Photo key={photo.id} photo={photo} />
             ) }
           </div> : <div />
         }
