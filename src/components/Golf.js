b3906f7c37b473b1e89ae4192e9d838a8f055856
Hannah Stahl <hls62@cornell.edu>
2019-01-03 10:23:40 -0600

    Updated player list

--- src/components/Golf.js
+++ src/components/Golf.js
@@ -14,39 +14,49 @@ class Golf extends Component {
             backgroundImage={require("../player-headshots/luke-list.png")}
           />
           <Card
-            athleteName="Zac Blair"
-            datesWithNic="since 2016"
-            backgroundImage={require("../player-headshots/zac-blair.png")}
-          />
-          <Card
-            athleteName="Boo Weekley"
-            datesWithNic="since 2015"
-            backgroundImage={require("../player-headshots/boo-weekley.png")}
-          />
-          <Card
-            athleteName="D.A. Points"
-            datesWithNic="" // get these from Nic
-            backgroundImage={require("../player-headshots/da-points.png")}
-          />
-          <Card
-            athleteName="Patrick Cantlay"
-            datesWithNic="" // get these from Nic
-            backgroundImage={require("../player-headshots/patrick-cantlay.png")}
-          />
-          <Card
             athleteName="Chris Kirk"
             datesWithNic="since 2017"
             backgroundImage={require("../player-headshots/chris-kirk.png")}
           />
           <Card
-            athleteName="Ben Crane"
-            datesWithNic="since 2017"
-            backgroundImage={require("../player-headshots/ben-crane.png")}
+            athleteName="Dylan Frittelli"
+            datesWithNic="since ?" // TODO get this from Nic
+            backgroundImage={require("../player-headshots/dylan-frittelli.png")}
+          />
+          <Card
+            athleteName="Tom LoveLady"
+            datesWithNic="since ?" // TODO get this from Nic
+            backgroundImage={require("../player-headshots/tom-lovelady.png")}
+          />
+          <Card
+            athleteName="D.A. Points"
+            datesWithNic="since ?" // TODO get this from Nic
+            backgroundImage={require("../player-headshots/da-points.png")}
+          />
+          <Card
+            athleteName="Patrick Cantlay"
+            datesWithNic="?" // TODO get this from Nic
+            backgroundImage={require("../player-headshots/patrick-cantlay.png")}
           />
           <Card
             athleteName="Steven Bowditch"
             datesWithNic="from 2015 to 2016"
             backgroundImage={require("../player-headshots/steven-bowditch.png")}
+          />
+          <Card
+            athleteName="Zac Blair"
+            datesWithNic="from 2016 to 2018"
+            backgroundImage={require("../player-headshots/zac-blair.png")}
+          />
+          <Card
+            athleteName="Boo Weekley"
+            datesWithNic="from 2015 to 2018"
+            backgroundImage={require("../player-headshots/boo-weekley.png")}
+          />
+          <Card
+            athleteName="Ben Martin"
+            datesWithNic="?" // TODO get this from Nic
+            backgroundImage={require("../player-headshots/ben-martin.png")}
           />
           <Card
             athleteName="Cameron Percy"
