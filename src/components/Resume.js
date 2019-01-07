bd3442a882f19b9ae249c1a048f5411fb6b1d337
Hannah Stahl <hls62@cornell.edu>
2019-01-07 17:24:59 -0600

    Added background image to resume section and updated styling more

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,53 +6,54 @@ class Resume extends Component {
   render() {
     return (
       <div>
+        <h1>Experience</h1>
         <div className="timeline">
           <div className="timeline-container left">
             <ResumeCard
-              eventName="Brisbane Roar"
+              eventName="PGA Tour"
               eventDetails="Details go here"
-              backgroundImage={require("../resume-images/brisbane-roar.png")}
+              backgroundImage={require("../resume-images/pga-tour.png")}
               resumeCardContainerClass="left-resume-card-container"
             />
           </div>
           <div className="timeline-container right">
             <ResumeCard
-              eventName="Brisbane Lions"
+              eventName="Sports Therapy Australia"
               eventDetails="Details go here"
-              backgroundImage={require("../resume-images/brisbane-lions.png")}
+              backgroundImage={require("../resume-images/sports-therapy-australia.jpeg")}
               resumeCardContainerClass="right-resume-card-container"
             />
           </div>
           <div className="timeline-container left">
+            <ResumeCard
+              eventName="Cricket Australia"
+              eventDetails="Details go here"
+              backgroundImage={require("../resume-images/cricket-australia.jpg")}
+              resumeCardContainerClass="left-resume-card-container"
+            />
+          </div>
+          <div className="timeline-container right">
             {/* TODO Replace with Health and Fitness Australia once you get that image */}
             <ResumeCard
               eventName="Brisbane Roar"
               eventDetails="Details go here"
               backgroundImage={require("../resume-images/brisbane-roar.png")}
-              resumeCardContainerClass="left-resume-card-container"
-            />
-          </div>
-          <div className="timeline-container right">
-            <ResumeCard
-              eventName="Cricket Australia"
-              eventDetails="Details go here"
-              backgroundImage={require("../resume-images/cricket-australia.jpg")}
               resumeCardContainerClass="right-resume-card-container"
             />
           </div>
           <div className="timeline-container left">
             <ResumeCard
-              eventName="Sports Therapy Australia"
+              eventName="Brisbane Lions"
               eventDetails="Details go here"
-              backgroundImage={require("../resume-images/sports-therapy-australia.jpeg")}
+              backgroundImage={require("../resume-images/brisbane-lions.png")}
               resumeCardContainerClass="left-resume-card-container"
             />
           </div>
           <div className="timeline-container right">
             <ResumeCard
-              eventName="PGA Tour"
+              eventName="Brisbane Roar"
               eventDetails="Details go here"
-              backgroundImage={require("../resume-images/pga-tour.png")}
+              backgroundImage={require("../resume-images/brisbane-roar.png")}
               resumeCardContainerClass="right-resume-card-container"
             />
           </div>
