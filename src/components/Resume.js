7f329f3b9802f6b2753c265b33feb04ac9614432
Hannah Stahl <hls62@cornell.edu>
2019-01-07 09:54:11 -0600

    Got rough timeline set up for resume section

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,7 +5,44 @@ class Resume extends Component {
   render() {
     return (
       <div>
-        Resume
+        <div className="timeline">
+          <div className="timeline-container left">
+            <div className="timeline-content">
+              <h2>2017</h2>
+              <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            </div>
+          </div>
+          <div className="timeline-container right">
+            <div className="timeline-content">
+              <h2>2016</h2>
+              <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            </div>
+          </div>
+          <div className="timeline-container left">
+            <div className="timeline-content">
+              <h2>2015</h2>
+              <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            </div>
+          </div>
+          <div className="timeline-container right">
+            <div className="timeline-content">
+              <h2>2012</h2>
+              <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            </div>
+          </div>
+          <div className="timeline-container left">
+            <div className="timeline-content">
+              <h2>2011</h2>
+              <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            </div>
+          </div>
+          <div className="timeline-container right">
+            <div className="timeline-content">
+              <h2>2007</h2>
+              <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
