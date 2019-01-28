1dde728be3a79cb9778538ee942fab58c30ee3b3
Hannah Stahl <hls62@cornell.edu>
2019-01-28 09:41:49 -0600

    Added Lorem Ipsum text for About Me

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ class About extends Component {
     return (
       <div>
         <h1>About Me</h1>
-        <p>Insert bio here</p>
+        <p className="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque cursus ornare euismod. In vel ligula non tortor fermentum interdum ut eu nisi. Donec ut iaculis arcu, sed euismod purus. In egestas tortor non consequat malesuada. Proin ut purus leo. Vestibulum leo metus, auctor ut tellus vitae, sollicitudin tincidunt odio. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed tincidunt risus vel lectus pretium, eu imperdiet felis vehicula. Sed lobortis sem eu nisl auctor tempus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut aliquet enim id porttitor fringilla. Pellentesque in bibendum arcu. Suspendisse vitae nibh placerat, scelerisque dolor vel, gravida diam. Fusce pulvinar elementum tortor, at vestibulum risus egestas ac. Integer fermentum velit vel ex volutpat, finibus suscipit tortor dapibus. Nam in molestie urna, ut eleifend elit. Fusce rhoncus ultrices diam ac tristique. Aliquam efficitur dictum sagittis. Fusce sed pellentesque augue. Nunc convallis velit est.</p>
       </div>
     );
   }
