4833aa7c091e93746fc6975bca04b2befa370ed9
Hannah Stahl <hls62@cornell.edu>
2019-02-10 20:15:40 -0600

    Filled in About section

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,22 @@ class About extends Component {
   render() {
     return (
       <div>
-        <h1>About Me</h1>
-        <p className="bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque cursus ornare euismod. In vel ligula non tortor fermentum interdum ut eu nisi. Donec ut iaculis arcu, sed euismod purus. In egestas tortor non consequat malesuada. Proin ut purus leo. Vestibulum leo metus, auctor ut tellus vitae, sollicitudin tincidunt odio. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed tincidunt risus vel lectus pretium, eu imperdiet felis vehicula. Sed lobortis sem eu nisl auctor tempus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut aliquet enim id porttitor fringilla. Pellentesque in bibendum arcu. Suspendisse vitae nibh placerat, scelerisque dolor vel, gravida diam. Fusce pulvinar elementum tortor, at vestibulum risus egestas ac. Integer fermentum velit vel ex volutpat, finibus suscipit tortor dapibus. Nam in molestie urna, ut eleifend elit. Fusce rhoncus ultrices diam ac tristique. Aliquam efficitur dictum sagittis. Fusce sed pellentesque augue. Nunc convallis velit est.</p>
+        <h1>About Nic</h1>
+        <p className="bio">
+          Nic Catterall is an Australian high-performance coach, 
+          specializing in Musculoskeletal Therapy and Strength & Conditioning 
+          for professional golfers on the PGA Tour in the U.S. 
+          With over five years of tour experience, plus 10 years of clinical 
+          and industry experience across multiple sporting codes, 
+          Nic takes a tailored approach to each athlete he works with. 
+          <br /><br />
+          Nic believes in training athletes to be formidable to their core - 
+          not just in performance, but in life. 
+          By seeking out areas for improvement, enhancing exceptional qualities, 
+          and encouraging fluid adaptation, Nic helps his clients succeed 
+          in an ever-changing world. He encourages hard and smart work 
+          aimed at developing body, brain, and character.
+        </p>
       </div>
     );
   }
